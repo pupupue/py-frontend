137088dc962a0d66437653da86949da2bd15cbfb
kristaps.greitans <kristaps.greitans@edi.lv>
2021-05-07 17:19:38 +0300

    login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,25 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./Reset.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+import reducer from "./store/reducers/auth";
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>
 );
+
+ReactDOM.render(app,document.getElementById("root"));
 
 reportWebVitals();
